99ca49fcb3aa099a2a39f863750c0cba7cd2b5af
AmZakirov <112470168+AmZakirov@users.noreply.github.com>
2024-02-14 13:55:03 +0300

    Update Cities.py

--- Bin/Elements/Cities.py
+++ Bin/Elements/Cities.py
@@ -175,8 +175,7 @@ class Cities(City_options):
                     if count_1 != 0:
                         player1.return_meeple(count_1)
                     
-                    player0.write()
-                    player1.write()
+                    player0.write([player0.players_list[0], player1.players_list[1]])
                         
                 
         json_write('Data/Elements/Cities.json', self.cities)
