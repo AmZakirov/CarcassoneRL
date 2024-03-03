9cb8ef36f88aa77db8f4f7ea7878f66ce440eebb
AmZakirov <112470168+AmZakirov@users.noreply.github.com>
2024-03-03 13:17:05 +0300

    добавил реварды

--- main.py
+++ main.py
@@ -30,46 +30,55 @@ def main():
     shuffle(all_tiles)
     
     # Цикл по выложенным тайлам:
-    for ind, tile_to_put in enumerate(all_tiles):
-        
-        player_id = ind % 2
-        # Получение списка из мест, куда можно положить тайл
-        placetile = PlaceTile(board.board, tile_to_put)
-        availible_places = placetile.availible_places()
-        
-        if len(availible_places) == 0:
-            pass #TODO: Переложить тайл в конец колоды
-        
+    unputted_tiles = []
+    flag = False
+    for j in range(2):
+        if len(unputted_tiles) == 0 and not flag:
+            tiles0 = all_tiles
+        elif len(unputted_tiles) == 0 and flag:
+            break
         else:
-            if player_id== 0:
-                place = choice(availible_places) # TODO: DECISION MAKING
+            tiles0 = unputted_tiles
+        for ind, tile_to_put in enumerate(tiles0):
+            
+            player_id = ind % 2
+            # Получение списка из мест, куда можно положить тайл
+            placetile = PlaceTile(board.board, tile_to_put)
+            availible_places = placetile.availible_places()
+            
+            if len(availible_places) == 0:
+                unputted_tiles.append(tile_to_put)
+            
             else:
-                place = choice(availible_places) # RANDOM
+                if player_id== 0:
+                    place = choice(availible_places) # TODO: DECISION MAKING
+                else:
+                    place = choice(availible_places) # RANDOM
+                    
+                # Вращение и добавление ограничения в тайл
+                tile_to_put = placetile.change_tile(tile_to_put, *place)
+                board.put_tile(tile_to_put)
                 
-            # Вращение и добавление ограничения в тайл
-            tile_to_put = placetile.change_tile(tile_to_put, *place)
-            board.put_tile(tile_to_put)
+                # Действия: 1- для пропуска, остальное-это объекты на поле, куда можно положить тайл
+                actions = board.add_action() + [1]
+                rewards = Rewards(actions, board.board).reward_list
+                
+                if player_id == 0:
+                    action_freedom = choice(actions)  #TODO: DECISION MAKING
+                else:
+                    action_freedom = choice(actions)  #RANDOM
+                
+                # Закрытие объектов на поле, дороги с 2-мя концами, города закрытые со всех сторон, закрытые церкви
+                board.close_action(action=action_freedom, player_ID=player_id) 
             
-            # Действия: 1- для пропуска, остальное-это объекты на поле, куда можно положить тайл
-            actions = board.add_action() + [1]
-            rewards = Rewards(actions, board.board).reward_list
-            
-            if player_id == 0:
-                action_freedom = choice(actions)  #TODO: DECISION MAKING
-            else:
-                action_freedom = choice(actions)  #RANDOM
-            
-            # Закрытие объектов на поле, дороги с 2-мя концами, города закрытые со всех сторон, закрытые церкви
-            board.close_action(action=action_freedom, player_ID=player_id)   
+            flag = True
     # Сохранение картинки 
-    plot_board(board.board)  
+    # plot_board(board.board)  
             
     # Подсчёт очков
     PointsClosed().write()  
     PointsUnclosed(board.board)
     
-        
-        
     # Очки за завершённые объекты:
     points_0 = json_read('Data/Players.json')[0]['Points']
     points_1 = json_read('Data/Players.json')[1]['Points']    
